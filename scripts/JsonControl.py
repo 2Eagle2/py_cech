131a3dd32cd0876f678112143a1e4e424dbb82d3
Unknown <Mi.Skrobak@gmail.com>
2018-02-15 16:24:54 +0100

    úprava komentárov

--- scripts/JsonControl.py
+++ scripts/JsonControl.py
@@ -6,15 +6,22 @@ import json
 
 
 def list_of_all():
-    """ výpiše zoznam všetkých dlžníkov aj výškou dlhu"""
-    with open("dlznici") as f:
+    """
+    výpiše zoznam všetkých dlžníkov aj výškou dlhu
+    :return:
+    """
+    with open("dlznici.json") as f:
         data = json.load(f)
         return data
 
 def list_one(name):
-    """vypíše cech zadaného dlžníka"""
+    """
+    vypíše cech zadaného dlžníka
+    :param name:
+    :return:
+    """
     if borrower_in_database(name):
-        with open("dlznici") as f:
+        with open("dlznici.json") as f:
             data = json.load(f)
 
     else:
@@ -22,24 +29,46 @@ def list_one(name):
 
 
 def new_borrower(name, value):
-    """pridá nového dlžníka do zoznamu"""
+    """
+    pridá nového dlžníka do zoznamu
+    :param name:
+    :param value:
+    :return:
+    """
     pass
 
 def takes_cech(name, value):
-    """zvýšenie cechu dlžníkovy"""
+    """
+    zvýšenie cechu dlžníkovy
+    :param name:
+    :param value:
+    :return:
+    """
     pass
 
 def return_cech(name, value):
-    """vrátenie cechu"""
+    """vrátenie cechu
+    :param name:
+    :param value:
+    :return:
+    """
     pass
 
 def delete_borrower(name):
-    """zmazanie dlžníka zo zonamu"""
+    """
+    zmazanie dlžníka zo zonamu
+    :param name:
+    :return:
+    """
     pass
 
 def borrower_in_database(name):
-    """kontrola či už je na zozname dlžnikov"""
-    with open("dlznici") as f:
+    """
+    kontrola či už je na zozname dlžnikov
+    :param name:
+    :return:
+    """
+    with open("dlznici.json") as f:
         data = json.load(f)
         if name in data:
             return True
@@ -47,3 +76,9 @@ def borrower_in_database(name):
             return False
 
 
+def delete_all():
+    """
+    zmazanie vsetkých dlžníkov
+    :return:
+    """
+    pass
